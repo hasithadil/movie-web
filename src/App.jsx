5d93b7e053793f4574af40d69a6952014d6110c6
hasithadil <kah.dilshan@gmail.com>
2025-08-27 19:04:26 +0530

    adding css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { Route, Routes } from "react-router-dom"
 import Home from "./assets/Pages/Home"
+import Favourite from "./assets/Pages/Favourite"
+import NavBar from "./assets/Components/NavBar"
+import "../src/css/App.css"
 
   function App() {
 
 
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourite />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
